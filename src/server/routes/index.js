bf69d50847353e52cebb44d0a857617449c29bf5
Christian Drengenberg <ChristianDrengenberg@googlemail.com>
2017-06-06 12:45:17 +0200

    Removed test endpoints.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -29,8 +29,6 @@ module.exports.init = function(app, db, config)
 
         app.post('/api/config/inchannel/current', (req, res) => this.addInChannelConfig(req, res, true));
         app.post('/api/config/inchannel', (req, res) => this.addInChannelConfig(req, res));
-        app.get('/api/test', (req, res) => res.json(req.opuscapita.userData()));
-        app.get('/api/test1', (req, res) => res.json('Test'));
     });
 }
 
