cb9e40e6b2cf8c4bdbdf5f65bb9fd124d0bb8c12
Lekealem Asong <lekealemasong@yahoo.de>
2017-07-06 12:49:48 +0200

    Modify webpack to bundle both main bundle and libraries bundle

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
-import ConnectSupplierWidget from './components/ConnectSupplierWidget.js';
 
 
-ReactDOM.render(<ConnectSupplierWidget actionUrl='http://localhost:8080' customerId='4' locale='de' />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
