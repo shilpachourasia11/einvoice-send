185ca6a1239302ff48cdd55914c0bbf390d3a6b6
lakshgoel95 <laksh@cronj.com>
2017-08-16 18:52:56 +0530

    accept onclick redirect to bnp/dashboard fixed

--- src/client/app.js
+++ src/client/app.js
@@ -141,7 +141,7 @@ export default class App extends React.Component
     }
 
     finalizeFlow = () => {
-        window.location.href = "/bnp";
+        window.location.href = "/bnp/dashboard";
     }
 
     render()
