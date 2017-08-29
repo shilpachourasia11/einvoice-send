ea2544bf4f4e14f95d0e970d5d264e811b0542f5
lakshgoel95 <laksh@cronj.com>
2017-08-29 14:27:28 +0530

    changes request complete. rejectionEmail updated in the databse

--- src/server/api/inChannelConfig.js
+++ src/server/api/inChannelConfig.js
@@ -133,7 +133,7 @@ module.exports.updateInChannelConfig = function(supplierId, config, returnConfig
     // Remove fields we do not want to be set from outside.
     [ 'type', 'createdOn', 'changedOn', 'createdBy' ].forEach(key => delete basicConfig[key]);
     // Copy required values to the extendedConfig as it is a plain object.
-    [ 'changedBy' ].forEach(key => extendedConfig[key] = basicConfig[key]);
+    [ 'changedBy','rejectionEmail' ].forEach(key => extendedConfig[key] = basicConfig[key]);
 
     // Override supplierId making sure it's the same on both.
     basicConfig.supplierId = supplierId;
