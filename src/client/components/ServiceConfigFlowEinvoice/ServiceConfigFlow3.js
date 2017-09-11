9e0f3ffac349c3c12237a268709ecc36891d8668
lakshgoel95 <laksh@cronj.com>
2017-09-11 18:01:13 +0530

    updated jsFilModule and moduleName for the service Component

--- src/client/components/ServiceConfigFlowEinvoice/ServiceConfigFlow3.js
+++ src/client/components/ServiceConfigFlowEinvoice/ServiceConfigFlow3.js
@@ -9,12 +9,19 @@ export default class ServiceConfigFlow3 extends React.Component {
 	InvoiceEditor = React.createClass({
 		componentWillMount() {
 			let serviceRegistry = (service)=> ({ url:'/invoice' });
-			const InvoiceEditorForm = serviceComponent({ serviceRegistry, serviceName: 'sales-invoice',moduleName:'InvoiceForm.react' });
+			const InvoiceEditorForm = serviceComponent({ 
+				serviceRegistry, 
+				serviceName: 'invoice',
+				moduleName:'invoice_form',
+				jsFileName: 'invoice_form-bundle'
+			});
 			this.externalComponents = { InvoiceEditorForm };
 		},
 		render() {
 			console.log('inside render funciton of inner react class');
+			console.log(this.externalComponents)
 			const { InvoiceEditorForm } = this.externalComponents;
+			console.log(InvoiceEditorForm)
 			return (
 				<div>
 					<div>
