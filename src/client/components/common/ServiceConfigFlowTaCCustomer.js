10bc664ccc4296ab3046797c85acc1018a89ace1
nczeranka <norbert.czeranka@opuscapita.com>
2017-09-19 17:27:06 +0200

    moved OC terms and conditions to blob (#92)

--- src/client/components/common/ServiceConfigFlowTaCCustomer.js
+++ src/client/components/common/ServiceConfigFlowTaCCustomer.js
@@ -38,7 +38,7 @@ export default class ServiceConfigFlow2 extends React.Component {
     };
 
 
-    componentWillMount() {
+    componentDidMount() {
         this.setTermsAndConditions(this.context.locale);
         this.loadInChannelContract();
     }
