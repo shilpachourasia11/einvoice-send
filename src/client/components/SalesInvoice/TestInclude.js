048e612db8a5c69b6eb533ac1de4cc640fb355c6
nczeranka <norbert.czeranka@opuscapita.com>
2017-10-16 14:20:13 +0000

    code cleansing

--- src/client/components/SalesInvoice/TestInclude.js
+++ src/client/components/SalesInvoice/TestInclude.js
@@ -5,8 +5,7 @@ import ajax from 'superagent-bluebird-promise';
 export default class KeyIn extends React.Component {
 
     static contextTypes = {
-        currentUserData : React.PropTypes.object.isRequired,
-        locale : React.PropTypes.string
+        currentUserData : React.PropTypes.object.isRequired
     };
 
 	constructor(props) {
@@ -22,20 +21,9 @@ export default class KeyIn extends React.Component {
             jsFileName: 'sales_invoice_test-bundle'
             // componentPath: 'Test'
         });
-
-        let ConnectSupplierWidget = serviceComponent({
-            serviceRegistry: (service) => ({ url: '/einvoice-send' }),
-            serviceName: 'einvoice-send',
-            moduleName: 'connect-supplier-widget'
-        });
-
-        this.component = { ConnectSupplierWidget }
     }
 
     render() {
-        let {ConnectSupplierWidget} = this.component;
-        console.log("this.context.locale: ", this.context.locale);
-
         return (
             <div>
                 <h1>Sales-Invoice Test Cross-Service-Include</h1>
@@ -44,14 +32,7 @@ export default class KeyIn extends React.Component {
                 <br/>
                 <br/>
 
-                <div className="panel-heading">InvoiceTest...</div>
-                <this.InvoiceTest userName="<extra Name>"/>
-
-                <div className="panel-heading">ConnectSupplierWidget...</div>
-                <div className="panel-body">
-                    {<ConnectSupplierWidget locale={"de"} actionUrl='' customerId='ncc'/>}
-                </div>
-
+                <this.InvoiceTest userName="<Name passed from TestInclude2>"/>
             </div>
         )
     }
