eb4f9f05abb83459f3b9280d7368bb28b6c39878
nczeranka <norbert.czeranka@opuscapita.com>
2017-10-20 14:28:40 +0000

    adjusted the number formatting

--- src/client/i18n/formatPatterns.js
+++ src/client/i18n/formatPatterns.js
@@ -3,7 +3,7 @@ module.exports = {
         datePattern: 'dd/MM/yyyy',
         dateTimePattern: 'dd/MM/yyyy HH:mm:ss',
         integerPattern: '#,##0',
-        numberPattern: '#,##0.00#######',
+        numberPattern: '#,##0.00',
         numberDecimalSeparator: '.',
         numberGroupingSeparator: ',',
         numberGroupingSeparatorUse: true
@@ -11,8 +11,8 @@ module.exports = {
     'de': {
         datePattern: 'dd.MM.yyyy',
         dateTimePattern: 'dd.MM.yyyy HH:mm:ss',
-        integerPattern: '#.##0',
-        numberPattern: '#.##0,00#######',
+        integerPattern: '#,##0',
+        numberPattern: '#,##0.00',
         numberDecimalSeparator: ',',
         numberGroupingSeparator: '.',
         numberGroupingSeparatorUse: true
